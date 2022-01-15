ec72ee3827d31242f8d4586562cfb52c944deff0
haffa <haffar.sadok@gmail.com>
2022-01-15 22:20:00 +0100

    FireBase and pop Movie App

--- app/src/main/java/com/example/firebase/MoviesApiService.java
+++ app/src/main/java/com/example/firebase/MoviesApiService.java
@@ -4,9 +4,10 @@ package com.example.firebase;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 
 public interface MoviesApiService {
     @GET("/movie/popular")
-    Call<Movie> getPopularMovies(String cb);
+    Call<Movie> getPopularMovies(@Query("API_KEY") String API_KEY);
 }
