4f4de6d0d52b254d1a6953796997d78ac5836709
haffa <haffar.sadok@gmail.com>
2022-01-25 03:11:48 +0100

    FireBase and pop Movie App

--- app/src/main/java/com/example/firebase/ProfileActivity.java
+++ app/src/main/java/com/example/firebase/ProfileActivity.java
@@ -1,131 +1,84 @@
 package com.example.firebase;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.firebase.database.core.view.View;
+import com.example.firebase.model.Constants;
+import com.example.firebase.model.MovieResponse;
+import com.example.firebase.model.Result;
+import com.example.firebase.retrofit.ApiService;
+import com.example.firebase.retrofit.RetrofitClient;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-
-import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private MoviesAdapter mAdapter;
-    private String API_KEY ="db67b9e2dc3ca56b56ca546f428526ac";
+    public static final String TAG = ProfileActivity.class.getSimpleName();
+    private MovieAdapter adapter;
+    private List<Result>movies;
+    private RecyclerView recyclerView;
+    TextView textTop, textPopular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_profile);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        mAdapter = new MoviesAdapter(this);
-        mRecyclerView.setAdapter(mAdapter);
+
+        textPopular = findViewById(R.id.text_popular);
+
         getPopularMovies();
     }
 
-    private <call> void getPopularMovies() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.themoviedb.org/3")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        MoviesApiService service = retrofit.create(MoviesApiService.class);
-        Call<Movie>call = service.getPopularMovies(API_KEY);
-        call.enqueue(new Callback<Movie>() {
+    private void getPopularMovies(){
+        ApiService service = RetrofitClient.getRetrofitClient().create(ApiService.class);
+        Call<MovieResponse>call = service.getPopularMovies(Constants.API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
             @Override
-            public void onResponse(Call<Movie> call, Response<Movie> response) {
-                mAdapter.setMovieList(Movie.MovieResult.getResults());
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                MovieResponse movieResponse = response.body();
+                movies = movieResponse.getResults();
+                initRecyclerView();
             }
-
             @Override
-            public void onFailure(Call<Movie> call, Throwable t) {
-                t.printStackTrace();
-
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Toast.makeText(ProfileActivity.this, TAG + "erreur fetching movies", Toast.LENGTH_LONG).show();
             }
         });
-
-    }
-
-    public static class MovieViewHolder extends RecyclerView.ViewHolder {
-        public ImageView imageView;
-        public MovieViewHolder(View itemView) {
-            super(itemView);
-            imageView = (ImageView)findViewById(R.id.imageView);
-        }
-    }
-    public static class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
-        private List<Movie> mMovieList;
-        private LayoutInflater mInflater;
-        private ProfileActivity mContext;
-
-        public MoviesAdapter(ProfileActivity context) {
-            this.mContext = context;
-            this.mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public MovieViewHolder onCreateViewHolder(ViewGroup parent, final int viewType) {
-            android.view.View view = mInflater.inflate(R.layout.row_movie, parent, false);
-            final MovieViewHolder viewHolder = new MovieViewHolder(view);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int position = viewHolder.getAdapterPosition();
-                    Intent intent = new Intent(mContext, MovieDetailActivity.class);
-                    intent.putExtra(MovieDetailActivity.EXTRA_MOVIE, mMovieList.get(position));
-                    mContext.startActivity(intent);
-                }
-            });
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(MovieViewHolder holder, int position) {
-            Movie movie = mMovieList.get(position);
-            Glide.with(mContext)
-                    .load(movie.getPoster())
-                    .placeholder(R.color.colorAccent)
-                    .into(holder.imageView);
-        }
-
-        @Override
-        public int getItemCount() {
-            return (mMovieList == null) ? 0 : mMovieList.size();
-        }
-
-        public void setMovieList(List<Movie> movieList) {
-            this.mMovieList = new ArrayList<>();
-            this.mMovieList.addAll(movieList);
-            notifyDataSetChanged();
-        }
     }
 
 
-
-
+    private void initRecyclerView(){
+        recyclerView = findViewById(R.id.recycler);
+        adapter = new MovieAdapter(this, movies);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        adapter.setOnClickListener(new MovieAdapter.OnClickListener() {
+            @Override
+            public void OnItemClick(int position) {
+                Toast.makeText(ProfileActivity.this, "Clicked at position: " + position, Toast.LENGTH_LONG).show();
+                Result result = movies.get(position);
+                Intent intent = new Intent(ProfileActivity.this, MovieActivity.class);
+                intent.putExtra(MovieActivity.KEY_MOVIE, result);
+                startActivity(intent);
+            }
+        });
+        adapter.notifyDataSetChanged();
+    }
 
 
 
 }
+
